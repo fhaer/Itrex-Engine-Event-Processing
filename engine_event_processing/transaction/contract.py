ea1170abf6242eac73413a69da5db583e2a1d453
Felix Härer <felix.haerer@mailbox.org>
2023-03-20 16:13:00 +0100

    Update contract.py
    new web3 api

--- engine_event_processing/transaction/contract.py
+++ engine_event_processing/transaction/contract.py
@@ -2,7 +2,7 @@ from web3 import Web3
 import json
 
 ADDRESS_STR = "0x678Dc3b6316c031478a5F7Ad4D4E611CE1915262"
-ADDRESS = Web3.toChecksumAddress(ADDRESS_STR)
+ADDRESS = Web3.to_checksum_address(ADDRESS_STR)
 
 ABI_FILE = "contract.json"
 SOL_FILE = "contract.sol"
